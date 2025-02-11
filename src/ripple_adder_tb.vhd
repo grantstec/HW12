b94ac03a7cb7737918b3d95eb57fa3adf0b92499
grantstec <129014437+grantstec@users.noreply.github.com>
2025-02-11 08:27:24 -0700

    code

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,6 +49,9 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= X"00"; w_Cin <='1'; wait for 10 ns;
+           assert (w_sum = x"1" and w_Cout = '1') report "zero inputes but Cin" severity failure;
+       
 	
 		wait; -- wait forever
 	end process;	
